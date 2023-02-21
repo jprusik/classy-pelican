73629876132be87d45e8919dfe2e61ad2d78db24
Jon Prusik <jprusik@classynemesis.com>
2023-02-21 17:27:26 -0500

    refactor github repo loading and bodypadding to not depend on jQuery

--- static/js/github.js
+++ static/js/github.js
@@ -1,42 +1,81 @@
-var github = (function(){
+var github = (function () {
   function escapeHtml(str) {
-    return $('<div/>').text(str).html();
+    var newDiv = document.createElement('div');
+    newDiv.innerHTML = str;
+    return newDiv.outerHTML;
   }
-  function render(target, repos){
-    var i = 0, fragment = '', t = $(target)[0];
+
+  function render(target, repos) {
+    var i = 0,
+      fragment = '',
+      t = document.querySelector(target);
+
     fragment += '<ul class="list-group" id="github">';
 
-    for(i = 0; i < repos.length; i++) {
-      fragment += '<li class="list-group-item"><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p><small>'+escapeHtml(repos[i].description||'')+'</small></p></li>';
+    for (i = 0; i < repos.length; i++) {
+      fragment +=
+        '<li class="list-group-item"><a href="' +
+        repos[i].html_url +
+        '">' +
+        repos[i].name +
+        '</a><p><small>' +
+        escapeHtml(repos[i].description || '') +
+        '</small></p></li>';
     }
     fragment += '</ul>';
     t.innerHTML = fragment;
   }
-  return {
-    showRepos: function(options){
-      $.ajax({
-          url: "https://api.github.com/users/"+options.user+"/repos?callback=?"
-        , dataType: 'jsonp'
-        , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
-        , success: function(data) {
-          var repos = [];
-          if (!data || !data.data) { return; }
-          for (var i = 0; i < data.data.length; i++) {
-            if (options.skip_forks && data.data[i].fork) { continue; }
-            repos.push(data.data[i]);
-          }
-          repos.sort(function(a, b) {
-            var aDate = new Date(a.pushed_at).valueOf(),
-                bDate = new Date(b.pushed_at).valueOf();
 
-            if (aDate === bDate) { return 0; }
-            return aDate > bDate ? -1 : 1;
-          });
+  function onError(error, options) {
+    var loadingElement = document.querySelector(`${options.target} .loading`);
+    loadingElement.classList.add('error');
+    loadingElement.innerText = 'There was an issue loading the repo list.';
+    loadingElement.classList.remove('loading');
+  }
 
-          if (options.count) { repos.splice(options.count); }
-          render(options.target, repos);
-        }
-      });
+  function onSuccess(data, options) {
+    var repos = [];
+
+    if (!data) {
+      return;
     }
+
+    for (var i = 0; i < data.length; i++) {
+      if (options.skip_forks && data.data[i].fork) {
+        continue;
+      }
+
+      repos.push(data[i]);
+    }
+
+    repos.sort(function (a, b) {
+      var aDate = new Date(a.pushed_at).valueOf(),
+        bDate = new Date(b.pushed_at).valueOf();
+
+      if (aDate === bDate) {
+        return 0;
+      }
+
+      return aDate > bDate ? -1 : 1;
+    });
+
+    if (options.count) {
+      repos.splice(options.count);
+    }
+
+    render(options.target, repos);
+  }
+
+  return {
+    showRepos: function (options) {
+      fetch(`https://api.github.com/users/${options.user}/repos`)
+        .then((response) => response.json())
+        .then(function (data) {
+          onSuccess(data, options);
+        })
+        .catch(function (error) {
+          onError(error, options);
+        });
+    },
   };
 })();
